aad31e97d2d539129fd4ac133658bebccf7ac163
DMalich <d.malich@gmail.com>
2023-01-17 21:12:20 +0200

    created fake data for testing ConversationsSidebar

---
+++ src/mocks/mockConversations.ts
@@ -0,0 +1,24 @@
+import { ConversationType } from "../utils/types";
+
+export default [
+    {
+        id: 1,
+        name: "Joe Burrow",
+        lastMessage: "How are you?",
+    },
+    {
+        id: 2,
+        name: "Hannah Hoffmann",
+        lastMessage: "See you at 9",
+    },
+    {
+        id: 3,
+        name: "Sam Hubbard",
+        lastMessage: "Let's go!",
+    },
+    {
+        id: 4,
+        name: "Wayne Rooney",
+        lastMessage: "What are your plans?",
+    },
+] as ConversationType[];
